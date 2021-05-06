c8dd6276d69b3e84e97665a043cd5cd914bc8c36
hazelisles <zhccj19@gmail.com>
2021-05-06 23:17:02 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const { static } = require('express')
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+
+mongoose.connect('mongodb://localhost/basic-login', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => console.log('mongodb error!'))
+db.once('open', () => console.log('mongodb connected!'))
 
 app.get('/', (req, res) => {
   return res.render('index')
