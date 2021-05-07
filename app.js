bd011211407d47873626c7ac4ff3c7b277e02895
hazelisles <zhccj19@gmail.com>
2021-05-07 14:21:40 +0800

    feat: add check account function

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const Users = require('./models/user')
+const checkAccount = require('./check')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
@@ -16,7 +18,19 @@ db.on('error', () => console.log('mongodb error!'))
 db.once('open', () => console.log('mongodb connected!'))
 
 app.get('/', (req, res) => {
-  return res.render('index')
+  const reset = { status: true }
+  return res.render('index', { result: reset })
+})
+
+app.post('/login', (req, res) => {
+  const account = req.body
+  const result = checkAccount(account)
+  if (result.status) {
+    return res.render('welcome', { result })
+  } else {
+    return res.render('index', { result })
+  }
+  
 })
 
 app.listen(3000, () => {
